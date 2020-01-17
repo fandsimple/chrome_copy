1dd2f09cb45e811dbed04363ca3576b20b46e1fa
fandsimple <fand1024@163.com>
2020-01-17 23:40:34 +0800

    csdn基本功能完成

--- js/contentJs/content.js
+++ js/contentJs/content.js
@@ -15,6 +15,12 @@ function main() {
         chrome.storage.sync.get({csdnFlag: 'off'}, function (data) {
             if (data.csdnFlag == 'on') {
                 injectCustomJs("js/export/csdn.js"); // 向页面中注入js文件，可以进行回调
+                //引入第三方库
+                var  turndown = document.createElement('script');
+                turndown.setAttribute('type', 'text/javascript');
+                turndown.src = 'https://unpkg.com/turndown/dist/turndown.js';
+                document.head.appendChild(turndown);
+
                 csdn();
             }
         });
--- js/export/csdn.js
+++ js/export/csdn.js
@@ -1,3 +1,20 @@
 function exportArticle() {
-    alert('csdn');
+    console.log('csdn exportArticle');
+    var title = '# ' +  $('div.article-title-box h1').text();
+    var content = $('div#article_content').html();
+    var turndownService = new window.TurndownService();
+    var markdown = turndownService.turndown(content);
+    markdown = title + '\n' + markdown;
+    $textarea_content = $('<textarea name="content" id="" cols="30" rows="10" style="position: absolute;top: 0;left: 0;opacity: 0;z-index: -10;">' + markdown + '</textarea>');
+    $('div.article-title-box').append($textarea_content);
+    $textarea_content.select();
+    var isSuccess = document.execCommand('copy');
+    if(isSuccess){
+        alert('导出文章到粘贴板成功！！！');
+    }else {
+        alert('导出文章内容失败，请重试！！！');
+    }
+    console.log(markdown);
+
+
 }
